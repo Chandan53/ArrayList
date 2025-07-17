a45bcd608d60d6bc5db373ab7218d7278e795045
chandan <chandankumar936672@gmail.com>
2025-07-17 16:21:53 +0530

    Introduction

---
+++ arraylist1.java
@@ -0,0 +1,23 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class arraylist1 {
+    public static void main(String args[]){
+        // Creating a ArrayList for Integer , String , float,boolean
+        // dynamic in size
+        // premetive data type cannot be store directly
+        // zero base indexing
+        ArrayList<Integer>list1=new ArrayList<>();
+        ArrayList<String>list2=new ArrayList<>();
+        ArrayList<Float>list3=new ArrayList<>();
+        ArrayList<Boolean>list4=new ArrayList<>();
+
+        // Operation in ArrayList & time complexity
+        // add element---> 0(1)
+        // get element -->0(1)
+        // remove element---> 0(n)
+        // set element in specific index-->0(n)
+        // contains element ---0(n)
+    }
+    
+}
