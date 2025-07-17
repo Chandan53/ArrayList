747870c938cc7c0b6dc5e39ea7cb5973f614dd79
chandan <chandankumar936672@gmail.com>
2025-07-17 16:48:39 +0530

    operation --> size()

---
+++ arraylist4.java
@@ -0,0 +1,16 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class arraylist4 {
+    public static void main(String args[]){
+        ArrayList<Integer>list1=new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+        System.out.println(list1.size());
+    }
+    
+}
